0d69772702c5c1a3ef721a1d0f949c91670c54b7
Raj patel <raj.patel@gantner.com>
2024-09-06 15:39:45 +0530

    intialize mars map

--- coding-kata/coding-kata-test/UnitTest1.cs
+++ coding-kata/coding-kata-test/UnitTest1.cs
@@ -10,6 +10,16 @@ public class Tests
     }
 
     [Test]
+    public void Initialize_Mars_Map()
+    {
+        // Act
+        var map = new Map();
+    
+        // Assert
+        Assert.That(map.MapPositions, Has.Count.EqualTo(25));
+    }
+
+    [Test]
     public void Rover_Has_Default_Position()
     {
         // Act
